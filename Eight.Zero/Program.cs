0a01c1c265b9054ffb3f066ab6c36e1466fc6318
InianAntony <inianantony.s@gmail.com>
2021-04-12 18:12:03 +0800

    Add case for purple, Orange, Brown

--- Eight.Zero/Program.cs
+++ Eight.Zero/Program.cs
@@ -71,7 +71,14 @@ namespace Eight.Zero
         {
             return (c1, c2) switch
             {
-                (Color.Red,Color.Blue) => Color.Purple
+                (Color.Red, Color.Blue) => Color.Purple,
+                (Color.Blue, Color.Red) => Color.Purple,
+
+                (Color.Yellow, Color.Red) => Color.Orange,
+                (Color.Red, Color.Yellow) => Color.Orange,
+
+                (Color.Red, Color.Green) => Color.Brown,
+                (Color.Green, Color.Red) => Color.Brown,
             };
         }
     }
