2b8e9dfc6934a7cfb1af8d9779be04abd513dc4c
InianAntony <inianantony.s@gmail.com>
2021-04-13 09:19:51 +0800

    Add static interface method

--- Eight.Zero/Program.cs
+++ Eight.Zero/Program.cs
@@ -10,6 +10,11 @@ namespace Eight.Zero
     public interface IAnimalWidget
     {
         private static int AmountToFeed = 10;
+
+        static void SetAmountToFeed(int amount)
+        {
+            AmountToFeed = amount;
+        }
         string Name { get; }
 
         int Happiness { get; set; }
